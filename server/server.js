25cda14ce5de791f4e431db4a15109d623947744
Liao jiexin <rarelyapple@gmail.com>
2025-02-14 09:57:38 +0800

    second commit

--- server/server.js
+++ server/server.js
@@ -3,19 +3,27 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
 import { clerkWebhooks } from './controllers/webhooks.js'
+import educatorRouter from './routes/educatorRoutes.js'
+import { clerkMiddleware } from '@clerk/express'
+import connectCloudinary from './configs/cloudinary.js'
+import courseRouter from './routes/courseRoute.js'
 
 //initialize express
 const app = express()
 
 //connect to database
 await connectDB()
+await connectCloudinary()
 
 //middlewares
 app.use(cors())
+app.use(clerkMiddleware())
 
 //routes
 app.get('/', (req,res) => res.send('api working'))
 app.post('/clerk', express.json(), clerkWebhooks)
+app.use('/api/educator', express.json(), educatorRouter)
+app.use('/api/course', express.json(),courseRouter)
 
 //port
 const PORT = process.env.PORT || 5000
