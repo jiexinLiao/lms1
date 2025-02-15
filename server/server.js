7faa49d8051fdc6224403cbdbcf51800e685c8d6
Liao jiexin <rarelyapple@gmail.com>
2025-02-15 11:31:06 +0800

    third commit

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import educatorRouter from './routes/educatorRoutes.js'
 import { clerkMiddleware } from '@clerk/express'
 import connectCloudinary from './configs/cloudinary.js'
 import courseRouter from './routes/courseRoute.js'
+import userRouter from './routes/userRoutes.js'
 
 //initialize express
 const app = express()
@@ -24,6 +25,7 @@ app.get('/', (req,res) => res.send('api working'))
 app.post('/clerk', express.json(), clerkWebhooks)
 app.use('/api/educator', express.json(), educatorRouter)
 app.use('/api/course', express.json(),courseRouter)
+app.use('/api/user',express.json(), userRouter)
 
 //port
 const PORT = process.env.PORT || 5000
